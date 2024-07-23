4514a6c8b50815c5a1ea667a8d4838e2251a8d00
shiverzm <1113673178@qq.com>
2024-07-24 01:02:04 +0800

    修复部分问题

--- start.sh
+++ start.sh
@@ -3,4 +3,6 @@ echo "
 |                   冒险岛079 FOR CentOS/Ubuntu/Debian
 +----------------------------------------------------------------------
 "
-./jdk/jre/bin/java -cp ./bin/maple.jar -server -DhomePath=./config/ -DscriptsPath=./scripts/ -DwzPath=./scripts/wz -Xms512m -Xmx2048m -XX:PermSize=256m -XX:MaxPermSize=512m -XX:MaxNewSize=512m server.Start
+MAPLE_JAR_PATH=/usr/local/java-projects/maplestory/
+export Path=$PATH:$MAPLE_JAR_PATH
+nohup java -cp ./MapleStory-079.jar -server -DhomePath=./config/ -DscriptsPath=./scripts/ -DwzPath=./scripts/wz -Xms512m -Xmx2048m -XX:PermSize=256m -XX:MaxPermSize=512m -XX:MaxNewSize=512m server.Start > start.log 2>&1 &
